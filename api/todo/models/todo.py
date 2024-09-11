002fd0b06fd9266d88884e4938889bab8ef2c20c
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-09-11 13:48:24 +0400

    feat: create todo model

---
+++ api/todo/models/todo.py
@@ -0,0 +1,12 @@
+class Todo:
+    def __init__(self, title, description, status="pending"):
+        self.title = title
+        self.description = description
+        self.status = status
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "description": self.description,
+            "status": self.status
+        }
