42b1558ed9662d43b8ab5ecf2f6a1dfb81c7397e
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-09-11 13:49:55 +0400

    feat: add create_todo controller

--- api/todo/controllers/todo_controller.py
+++ api/todo/controllers/todo_controller.py
@@ -5,5 +5,15 @@ db = get_db()
 todos_collection = db.todos
 
 
+def create_todo(data):
+    new_todo = Todo(
+        title=data.get('title'),
+        description=data.get('description')
+    )
+    inserted_id = todos_collection.insert_one(new_todo.to_dict()).inserted_id
+
+    return todos_collection.find_one({"_id": inserted_id})
+
+
 def get_all_todos():
     return list(todos_collection.find({}))
